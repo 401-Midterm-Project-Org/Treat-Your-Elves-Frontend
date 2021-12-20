04596bf7cdc28c915b0681ff7234ca5981f3234e
Chattray <chattray.chea@gmail.com>
2021-12-20 11:33:27 -0800

    multi-placeholders added to Main.js

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,18 +3,46 @@ import Box from '@mui/material/Box';
 
 export default function Main() {
   return(
+    <div>
 <Box
       sx={{
-        width: 2400,
-        height: 1095,
+        width: 300,
+        height: 300,
         backgroundColor: 'secondary.dark',
         '&:hover': {
           backgroundColor: 'secondary.main',
           opacity: [0.9, 0.8, 0.7],
         },
       }}
-    >
+      >
       To contain Group Members/Recipient/Wishlist/Admin/Group Number(Name)
     </Box>
+    <Box
+    sx={{
+      width: 500,
+      height: 500,
+      backgroundColor: 'primary.dark',
+      '&:hover': {
+        backgroundColor: 'primary.main',
+        opacity: [0.9, 0.8, 0.7],
+      },
+    }}
+  >
+    To contain Group Members/Recipient/Wishlist/Admin/Group Number(Name)
+  </Box>
+  <Box
+    sx={{
+      width: 800,
+      height: 300,
+      backgroundColor: 'secondary.dark',
+      '&:hover': {
+        backgroundColor: 'secondary.main',
+        opacity: [0.9, 0.8, 0.7],
+      },
+    }}
+  >
+    To contain Group Members/Recipient/Wishlist/Admin/Group Number(Name)
+  </Box>
+    </div>
   )
 }
