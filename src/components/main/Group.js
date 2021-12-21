e5deb2cd0c6d6f6e53041896f3639e7904ddac4b
Joe Ivans <contact@joeivans.dev>
2021-12-21 09:49:51 -0800

    - Updated group component, used MUI list, useState

--- src/components/main/Group.js
+++ src/components/main/Group.js
@@ -1,21 +1,27 @@
 import {Button, Typography} from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import * as React from 'react';
 
 
 export default function Group({myGroups}) {
-  const _myGroups = myGroups ? myGroups : ['default1', 'default2'];
-
   return (
     <>
-      <Typography variant="h4" component="div">
-        Group
-      </Typography>
+      <Typography variant="h4" component="div">My Groups</Typography>
       <Button variant="contained">+ Create Group</Button>
-      <ul>
+      <List>
         {
-          _myGroups.map(group => <li>{group}</li>)
+          myGroups.map(group => (
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary={group}/>
+              </ListItemButton>
+            </ListItem>
+          ))
         }
-      </ul>
+      </List>
     </>
   );
 }
