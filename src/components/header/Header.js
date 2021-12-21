a782a380510bf536dd35ddebf430aa8606ef132f
Justin Hamerly <suvimountain@icloud.com>
2021-12-21 07:11:29 -0800

    header configuration

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,34 +1,46 @@
 import * as React from 'react';
-import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@mui/material/';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@mui/material/";
 import MenuIcon from '@mui/icons-material/Menu';
 import AddCircleTwoToneIcon from '@mui/icons-material/AddCircleTwoTone';
 import LoginTwoToneIcon from '@mui/icons-material/LoginTwoTone';
 import LogoutTwoToneIcon from '@mui/icons-material/LogoutTwoTone';
 import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
 
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../../theme/theme'
+
 export default function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Treat Your Elves
-          </Typography>
-          <Button color="inherit"><AddCircleTwoToneIcon />SIGN-UP</Button>
-          <Button color="inherit"><LoginTwoToneIcon />LOGIN</Button>
-          <Button color="inherit"><AccountCircleTwoToneIcon />ACCOUNT</Button>
-          <Button color="inherit"><LogoutTwoToneIcon />LOGOUT</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" color="light" sx={{ flexGrow: 1 }}>
+              Treat Your Elves
+            </Typography>
+            <Button color="light"><AddCircleTwoToneIcon />SIGN-UP</Button>
+            <Button color="light"><LoginTwoToneIcon />LOGIN</Button>
+            <Button color="light"><AccountCircleTwoToneIcon />ACCOUNT</Button>
+            <Button color="light"><LogoutTwoToneIcon />LOGOUT</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
   );
 }
---
+++ src/theme/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#064C3F',
+      },
+      secondary: {
+        main: '#412528',
+        light: '#EFD2D8',
+      },
+      light: {
+        main: '#F1F8F3',
+      },
+    },
+});
+
+export default theme;
