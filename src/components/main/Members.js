1e4652cea747e473ba29bf3bc2f25d5d14906ffb
Chattray <chattray.chea@gmail.com>
2021-12-22 10:10:05 -0800

    regroup members state to Main.js

--- src/components/main/Members.js
+++ src/components/main/Members.js
@@ -1,16 +1,18 @@
 import {Button, Typography} from '@mui/material';
 import * as React from 'react';
 
-export default function Members({members}) {
-  const _members = members ? members : ['default1', 'default2'];
+
+
+
+
+export default function Members({myMembers}) {
   return(
     <>
     <Typography variant="h4" component="div">
       Elves
     </Typography>
-
     <ul>
-       {_members.map(member => <li>{member}<Button variant="contained" size="small">Wishlist</Button></li>)}
+       {myMembers.map(member => <li>{member}<Button variant="contained" size="small">Wishlist</Button></li>)}
     </ul>
     </>
   )
