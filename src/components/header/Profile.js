1fb7eea77aac6506f439714d9b88c68da01a3dfe
Joe Ivans <contact@joeivans.dev>
2021-12-28 02:23:51 -0800

    - Added profile dependency injection, conditional render

--- src/components/header/Profile.js
+++ src/components/header/Profile.js
@@ -1,23 +1,22 @@
-// import * as React from 'react';
+// todo: need a profile store / reducer
+const Profile = ({isLoading, isAuthenticated, user}) => {
+    if (isLoading) {
+    return (
+      <div>
+        loading...
+      </div>
+    );
+  }
 
-// const Profile = () => {
-//     if (isLoading) {
-//     return (
-//       <div>
-//         loading...
-//       </div>
-//     );
-//   }
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+};
 
-//   return (
-//     isAuthenticated && (
-//       <div>
-//         <img src={user.picture} alt={user.name} />
-//         <h2>{user.name}</h2>
-//         <p>{user.email}</p>
-//       </div>
-//     )
-//   );
-// };
-
-// export default Profile;
+export default Profile;
