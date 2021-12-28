5e1bf0b750bb3cbecb9f253fe16bd6ce953e236c
Joe Ivans <contact@joeivans.dev>
2021-12-28 02:18:56 -0800

    - Removed explicit props injection

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,29 @@
-import * as React from 'react';
-
-import {
-  AppBar,
-  Box,
-  Toolbar,
-  Typography,
-  IconButton,
-} from "@mui/material/";
 import MenuIcon from '@mui/icons-material/Menu';
+import {AppBar, Box, IconButton, Toolbar, Typography,} from '@mui/material';
 import HeaderButtons from './HeaderButtons';
 
 
-
-export default function Header(props) {
+export default function Header() {
   return (
-    <Box  sx={{ flexGrow: 1 }}>
-      <AppBar position="static" >
-        <Toolbar >
+    <Box sx={{flexGrow: 1}}>
+      <AppBar position="static">
+        <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
-            >
-            <MenuIcon />
+            sx={{mr: 2}}
+          >
+            <MenuIcon/>
           </IconButton>
-          <Typography variant="h3" component="div" color="light" sx={{ flexGrow: 1 }}>
-            <img src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/000000/external-elf-christmas-vitaliy-gorbachev-lineal-vitaly-gorbachev.png" alt="logo"/>
+          <Typography variant="h3" component="div" color="light" sx={{flexGrow: 1}}>
+            <img
+              src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/000000/external-elf-christmas-vitaliy-gorbachev-lineal-vitaly-gorbachev.png"
+              alt="logo"/>
             Treat Your Elves
           </Typography>
-          <HeaderButtons current={props} />
+          <HeaderButtons/>
         </Toolbar>
       </AppBar>
     </Box>
