cdf8ef803f008399e57250d84f3605df71cec575
Joe Ivans <contact@joeivans.dev>
2021-12-28 11:56:11 -0800

    - Add redux state to Profile component

--- src/components/header/Logout.js
+++ src/components/header/Logout.js
@@ -1,12 +1,32 @@
 import LogoutTwoToneIcon from '@mui/icons-material/LogoutTwoTone';
 import {Button} from '@mui/material';
+import {connect} from 'react-redux';
 
 
-const handleLogout = () => {
-  // todo: logout user
-  // todo: dispatch logout state
-};
+const mapStateToProps = (state) => ({
+  // todo: need any state?
+});
 
-export default function Logout() {
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => {
+    // todo: async/await logout on backend --> invalidate token
+
+    dispatch({
+      type: 'USER_LOGOUT',
+      payload: {
+        isLoggedIn: false,
+        username: null,
+        token: null,
+        id: null,
+      },
+    });
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(function Logout({logout}) {
+  const handleLogout = () => {
+    logout();
+  };
+
   return <Button color="secondary" onClick={handleLogout}><LogoutTwoToneIcon/>LOGOUT</Button>;
-}
+});
