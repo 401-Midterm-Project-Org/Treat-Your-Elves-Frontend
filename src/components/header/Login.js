7b00eb18397ef1626fbd0636a4af75aecdb43a43
Joe Ivans <contact@joeivans.dev>
2021-12-28 02:22:36 -0800

    - Removed explicit props injection, extracted handleLogin func for adding redux

--- src/components/header/Login.js
+++ src/components/header/Login.js
@@ -1,16 +1,8 @@
-import * as React from "react";
-import {
-  FormGroup,
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  Input,
-  Button,
-  Modal,
-  Box
-} from "@mui/material";
 import LoginTwoToneIcon from '@mui/icons-material/LoginTwoTone';
-import HttpService from "../../services/httpService";
+import {Box, Button, FormControl, FormGroup, FormHelperText, Input, InputLabel, Modal} from '@mui/material';
+import {useState} from 'react';
+import HttpService from '../../services/httpService';
+
 
 const style = {
   position: 'absolute',
@@ -24,41 +16,49 @@ const style = {
   p: 4,
 };
 
-export default function Login(props) {
+export default function Login() {
 
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    HttpService.login(username, password);
+  };
+
   return (
     <>
-    <Button color="secondary" onClick={handleOpen}><LoginTwoToneIcon />LOGIN</Button>
-    <Modal
-    open={open}
-    onClose={handleClose}
-    >
-    <Box sx={style}>
-    <FormGroup>
-      <FormControl>
-        <InputLabel htmlFor="my-input">User Name</InputLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {setUsername(e.target.value)}}/>
-        <FormHelperText id="my-helper-text">eg. your email</FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="my-input">Password</InputLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {setPassword(e.target.value)}}/>
-        <FormHelperText id="my-helper-text">
-          We'll never share your password.
-        </FormHelperText>
-      </FormControl>
-      <Button onClick={() => HttpService.login(username, password)}>Login</Button>
-      <p>Don't have an account? Register!</p>
-    </FormGroup>
-    </Box>
-    </Modal>
+      <Button color="secondary" onClick={handleOpen}><LoginTwoToneIcon/>LOGIN</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box sx={style}>
+          <FormGroup>
+            <FormControl>
+              <InputLabel htmlFor="my-input">User Name</InputLabel>
+              <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {
+                setUsername(e.target.value);
+              }}/>
+              <FormHelperText id="my-helper-text">eg. your email</FormHelperText>
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">Password</InputLabel>
+              <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {
+                setPassword(e.target.value);
+              }}/>
+              <FormHelperText id="my-helper-text">
+                We'll never share your password.
+              </FormHelperText>
+            </FormControl>
+            <Button onClick={handleLogin}>Login</Button>
+            <p>Don't have an account? Register!</p>
+          </FormGroup>
+        </Box>
+      </Modal>
     </>
   );
 }
