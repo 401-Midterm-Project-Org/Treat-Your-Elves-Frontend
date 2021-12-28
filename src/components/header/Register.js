1b980a02a03aa892e96fa37012bc83a330faa301
Joe Ivans <contact@joeivans.dev>
2021-12-28 02:24:46 -0800

    - Removed explicit props injection, extracted handleRegister func for redux

--- src/components/header/Register.js
+++ src/components/header/Register.js
@@ -1,16 +1,8 @@
-import * as React from "react";
-import {
-  FormGroup,
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  Input,
-  Button,
-  Modal,
-  Box
-} from "@mui/material";
 import LoginTwoToneIcon from '@mui/icons-material/LoginTwoTone';
-import HttpService from "../../services/httpService";
+import {Box, Button, FormControl, FormGroup, FormHelperText, Input, InputLabel, Modal} from '@mui/material';
+import {useState} from 'react';
+import HttpService from '../../services/httpService';
+
 
 const style = {
   position: 'absolute',
@@ -24,46 +16,54 @@ const style = {
   p: 4,
 };
 
-export default function Register(props) {
+export default function Register() {
 
-  const [open, setOpen] = React.useState(false);
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [name, setName] = React.useState('');
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  
-  // console.log(username)
+  const handleRegister = () => {
+    HttpService.register(username, name, password);
+  };
+
   return (
     <>
-    <Button color="secondary" onClick={handleOpen}><LoginTwoToneIcon />REGISTER</Button>
-    <Modal
-    open={open}
-    onClose={handleClose}
-    >
-    <Box sx={style}>
-    <FormGroup>
-      <FormControl>
-        <InputLabel htmlFor="my-input">User Name</InputLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {setUsername(e.target.value)}}/>
-        <FormHelperText id="my-helper-text">eg. JohnSmith</FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="my-input">Name</InputLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {setName(e.target.value)}}/>
-        <FormHelperText id="my-helper-text">eg. John Smith</FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="my-input">Password</InputLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {setPassword(e.target.value)}}/>
-        <FormHelperText id="my-helper-text">
-          We'll never share your password.
-        </FormHelperText>
-      </FormControl>
-      <Button onClick={() => HttpService.register(username, name, password)} >Register</Button>
-    </FormGroup>
-    </Box>
-    </Modal>
+      <Button color="secondary" onClick={handleOpen}><LoginTwoToneIcon/>REGISTER</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box sx={style}>
+          <FormGroup>
+            <FormControl>
+              <InputLabel htmlFor="my-input">User Name</InputLabel>
+              <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {
+                setUsername(e.target.value);
+              }}/>
+              <FormHelperText id="my-helper-text">eg. JohnSmith</FormHelperText>
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">Name</InputLabel>
+              <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {
+                setName(e.target.value);
+              }}/>
+              <FormHelperText id="my-helper-text">eg. John Smith</FormHelperText>
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">Password</InputLabel>
+              <Input id="my-input" aria-describedby="my-helper-text" onChange={(e) => {
+                setPassword(e.target.value);
+              }}/>
+              <FormHelperText id="my-helper-text">
+                We'll never share your password.
+              </FormHelperText>
+            </FormControl>
+            <Button onClick={handleRegister}>Register</Button>
+          </FormGroup>
+        </Box>
+      </Modal>
     </>
   );
 }
