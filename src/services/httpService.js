3561ea3098296672ad7b3181264283e568f0ac48
Justin Hamerly <suvimountain@icloud.com>
2021-12-28 11:18:20 -0800

    http methods written

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -53,11 +53,6 @@ class HttpService {
       new Error(`couldn't create group... ${e}`);
     }
   }
-  // ^^^ returns an object with 2 things:
-  // {
-  //   group: {id: 1, groupName: 'test', groupAdminId: 1 }
-  //   groupAssociation: {id: 1, groupId: group.id, userId: group.groupAdminId}
-  // }
   
   static async deleteGroup(groupId) {
     try{
@@ -81,48 +76,85 @@ class HttpService {
   }
 
   static async getUsersGroups(associationId) {
-    const {data} = await axios.get(`${APP_URL}/associations/${associationId}`);
-    return data;
+    try{
+      const data = await axios.get(`${APP_URL}/associations/${associationId}`);
+      return data;
+    }catch(e){
+      new Error(`couldn't get groups... ${e}`);
+    }
   }
 
-// get group members
+  static async getName(userid){
+    try{
+      const data = await axios.get(`${APP_URL}/name/${userid}`);
+      return data;
+    }catch(e){
+      new Error(`couldn't get user's name... ${e}`);
+    }
+  }
+
   static async getGroupMembers(groupId) {
-    // get all group assocations
-    // returs array with group members objects that looks like this
-    // {
-    //   id:
-    //   groupId:
-    //   userId:
-    //   role:
-    // }
-    // map over assocation objects and return a new array with the user object and the association id
-
-    const groupAssociations = await axios.get(`${APP_URL}/groupmembers/${groupId}`);
-    const users = groupAssociations.map(association => {
-      const userobj = await axios.get()
-    })
-
-
-
-    const USERS_URL = `${APP_URL}/${groupId}`;
-
-    return new Promise((resolve, reject) => resolve(['member1', 'member2']));
+    try{
+      const groupAssociations = await axios.get(`${APP_URL}/groupmembers/${groupId}`);
+      return groupAssociations;
+    }catch(e){
+      new Error(`couldn't get members... ${e}`);
+    }
   }
 
-// add member to group
-  static async addMember(email, groupId) {
-    const {data} = await axios.post(APP_URL + `/associations/${groupId}/${email}`);
-    return data;
+  static async addMember(groupId, username) {
+    try{
+      const {data} = await axios.post(APP_URL + `/associations/${groupId}/${username}`);
+      return data;
+    }catch(e){
+      new Error(`couldn't add member... ${e}`);
+    }
   }
 
-// remove member from group
+  static async removeMember(groupId, userid){
+    try{
+      await axios.delete(`${APP_URL}/groups/${groupId}/${userid}`);
+      return 'successful delete';
+    }catch(e){
+      new Error(`couldn't delete member... ${e}`);
+    }
+  }
 
 // create a wishlist item
+  static async addWishlistItem(associationsId, itemName){
+    try{
+      let item = await axios.post(APP_URL + `/listItem`, {
+        itemName,
+        associationsId
+      });
+      return item;
+    }catch(e){
+      new Error(`couldn't add item to wishlist... ${e}`);
+    }
+  }
 
 // update a wishlist item
+  static async updateItem(newItem, wishlistid, associationsId){
+    try{
+      const data = await axios.put(`${APP_URL}/listItem/${wishlistid}`, {
+        itemName: newItem,
+        associationsId,
+      });
+      return data;
+    }catch(e){
+      new Error(`couldn't update group name... ${e}`);
+    }
+  }
 
 // create secret santa pairing
-
+  static async createPairs(groupid){
+    try{
+      await axios.post(`${APP_URL}/santa/${groupid}`);
+      return 'successful pairing';
+    }catch(){
+      new Error(`couldn't pair santas... ${e}`);
+    }
+  }
 
 }
 
