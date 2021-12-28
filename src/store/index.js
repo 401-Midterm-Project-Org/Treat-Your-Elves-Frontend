5a0932867cad6f79d209e3fae029cd33a2debb3c
Joe Ivans <contact@joeivans.dev>
2021-12-28 03:08:40 -0800

    - Added userReducer to the redux store for logged in user info

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from 'redux';
 import myGroupsReducer from './reducers/myGroupsReducer.js';
+import userReducer from './reducers/userReducer';
 // import groupMemberReducer from './reducers/groupMembersReducer.js'
 // import wishlistReducer from './reducers/wishlistReducer.js';
 
 const reducers = combineReducers({
   myGroups: myGroupsReducer,
+  user: userReducer,
   // members: groupMemberReducer,
   // wishlist: wishlistReducer
 });
