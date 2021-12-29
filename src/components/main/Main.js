802a3a1c07f3fe6262ec14b29f127e94560be9e7
Chattray <chattray.chea@gmail.com>
2021-12-28 17:36:08 -0800

    swapped Box with grid in main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,4 +1,4 @@
-import {Box, Paper, Typography} from '@mui/material';
+import { Paper, Grid, Box } from '@mui/material';
 import React from 'react';
 import {connect} from 'react-redux';
 import Image from '../../theme/pixel-xmas.gif';
@@ -14,38 +14,35 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   //
-});
+}); 
 
 export default connect(mapStateToProps, mapDispatchToProps)(function Main({isLoggedIn, isAdministrator}) {
   const styles = {
     paperContainer: {
       backgroundImage: `url(${Image})`,
       width: '100%',
-      height: 757,
+      height: 800,
     }
   };
 
   return (
-    <main>
-      <Box id="maincontent" sx={{ justifycontent: 'center', width: 1, height: 1, backgroundColor: 'light.main', color: 'secondary.main', m: 1, p: 2, }} >
-        <Box sx={{ backgroundColor: 'third.main', height: 1, width: 1, }} id="groupinterface">
-          <Typography sx={{fontSize: 30}}>GROUP</Typography>
+    <main xs={8} >
           <Paper style={styles.paperContainer}>
+      <Grid container sx={{mt:'5%', justifyContent:'center'}}>
             {isLoggedIn &&
-              <div id="toolbar">
-              <Box sx={{ m: 3, padding: 1, width: 250, height: 717, backgroundColor: 'light.main', color: 'primary.main', }} >
+              <>
+              <Grid item sx={{ m: 3, padding: 1, width: 250, height: 500, backgroundColor: 'light.main', color: 'primary.main', borderRadius: 5}} >
                 <Members/>
-                <Box sx={{ ml: 130, padding: 1, width: 450, height: 100, backgroundColor: 'light.main', fontSize: 12, color: 'black', }} >
-                  <Admin/>
-                </Box>
-                <Box sx={{ ml: 130, mt: 30, padding: 1, width: 450, height: 160, backgroundColor: 'primary.main', fontSize: 12, color: 'white', }} >
+              </Grid>
+              <Grid item sx={{  m: 3, padding: 1, width: 450, height: 95, backgroundColor: 'light.main', fontSize: 12, color: 'black', borderRadius: 5 }} >
+                <Admin/>
+                <Box item sx={{ backgroundColor: 'primary.main', mt: 2, borderRadius: 5, color: 'white',  fontSize: 12, height: 200 }} >
                   <Recipient/>
                 </Box>
-              </Box>
-              </div>}
+              </Grid>
+              </>}
+    </Grid>
           </Paper>
-        </Box>
-      </Box>
     </main>
   );
 });
