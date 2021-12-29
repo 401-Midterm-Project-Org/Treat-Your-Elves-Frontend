a8cc7334b5bcf8e2138b1222ccb5db469fc7fc8b
Joe Ivans <contact@joeivans.dev>
2021-12-29 03:26:06 -0800

    - Added logged in, group clicked, group created actions to the reducer

--- src/store/reducers/storeReducer.js
+++ src/store/reducers/storeReducer.js
@@ -1,21 +1,31 @@
 const initialState = {
   user: {
-    isLoggedIn: true,
-    isAdministrator: false,
-    username: null,
-    email: null,
-    picture: null,
+    // id: null,
+    // token: null,
+    // isLoggedIn: false,
+    // username: null,
+    // email: null,
+    // picture: null,
   },
   loadStatus: {
     isLoading: true,
   },
   groups: [{
-    //
-    id: 0
-  }],
-  members: [{
-    //
-    id: 0
+    id: crypto.randomUUID(),
+    groupName: 'inb4',
+    // groupMembers: null,
+    // isSelected: true,
+  }, {
+    id: crypto.randomUUID(),
+    groupName: 'second!',
+    groupMembers: [{
+      id: crypto.randomUUID(),
+      name: 'testmember1'
+    }, {
+      id: crypto.randomUUID(),
+      name: 'testmember2'
+    }],
+    // isSelected: true,
   }],
 };
 
@@ -27,10 +37,44 @@ export default function storeReducer(state = initialState, action) {
     case 'USER_REGISTERED':
       return {
         ...state,
-        payload,
+        user: {
+          ...state.user,
+          id: payload.user.id,
+          token: payload.user.token,
+          isLoggedIn: payload.user.isLoggedIn,
+          username: payload.user.username,
+          name: payload.user.name,
+          email: payload.user.email,
+        },
+        loadStatus: {
+          ...state.loadStatus,
+          isLoading: payload.loadStatus.isLoading,
+        },
       };
     case 'USER_LOGOUT':
       return initialState;
+    case 'GROUP_CLICKED':
+      return {
+        ...state,
+        groups: state.groups.map(group => {
+          if (group.id === payload.group.id) {
+            return {...group, isSelected: true};
+          }
+          return {...group, isSelected: false};
+        }),
+      };
+    case 'GROUP_CREATED':
+      if (state.groups.some(g => g.groupName === payload.group.groupName)) {
+        return state;
+      }
+
+      return {
+        ...state,
+        groups: [
+          ...state.groups,
+          payload.group,
+        ],
+      };
     default:
       return state;
   }
